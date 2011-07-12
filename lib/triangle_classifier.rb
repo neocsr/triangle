525f925612db65b6e68c13393d557b7d475da5b3
Christian Sousa <neocsr@gmail.com>
2011-07-12 18:28:11 -0500

    Basic classification working.

---
+++ lib/triangle_classifier.rb
@@ -0,0 +1,29 @@
+# TriangleClassifier
+
+class TriangleClassifier
+  def self.classify(a, b, c)
+
+    check_sides(a, b, c)
+
+    case [a, b, c].uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else        :scalene
+    end
+
+  end
+
+  private
+
+  def self.check_sides(a, b, c)
+    sides = [a, b, c].sort
+
+    if sides.any? { |s| s <= 0 }
+      raise ArgumentError
+    end
+
+    if sides[0] + sides[1] <= sides[2]
+      raise ArgumentError
+    end
+  end
+end
---
+++ spec/triangle_classifier_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.dirname(__FILE__) + '/../lib/triangle_classifier.rb')
+
+describe "TriangleClassifier" do
+
+  it "should exist" do
+    TriangleClassifier.new.should_not be_nil
+  end
+
+  context "invalid length sides" do
+    it "should raise error when any length is zero or negative" do
+      expect{TriangleClassifier.check_sides(0, 5, 5)}.to raise_error(ArgumentError)
+    end    
+    
+    it "should raise error when length of the largest side greater than the sum of the others" do
+      expect{TriangleClassifier.check_sides(5, 5, 11)}.to raise_error(ArgumentError)
+    end    
+  end
+
+
+  context "valid length sides" do
+    it "should detect an equilateral triangle" do
+      TriangleClassifier.classify(5,5,5).should eql(:equilateral)
+    end
+
+    it "should detect an isosceles triangle" do
+      TriangleClassifier.classify(1,5,5).should eql(:isosceles)
+    end
+
+    it "should detect an scalene triangle" do
+      TriangleClassifier.classify(3,7,5).should eql(:scalene)
+    end
+  end
+
+end
