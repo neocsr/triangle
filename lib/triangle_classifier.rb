e2fd4151a9f9bad07151047642ea171e3c4395d6
Christian Sousa <neocsr@gmail.com>
2011-07-12 20:24:13 -0500

    Adding check when numeric types are different (Fixnum, Float).

--- lib/triangle_classifier.rb
+++ lib/triangle_classifier.rb
@@ -1,8 +1,11 @@
 # TriangleClassifier
 
+require 'bigdecimal'
+
 class TriangleClassifier
   def self.classify(a, b, c)
 
+    a, b, c = self.check_inputs(a, b, c)
     self.check_sides(a, b, c)
 
     case [a, b, c].uniq.size
@@ -24,6 +27,14 @@ class TriangleClassifier
 
     if sides[0] + sides[1] <= sides[2]
       raise ArgumentError
+    end
+  end
+
+  def self.check_inputs(a, b, c)
+    if [a, b, c].map{|e| e.class}.uniq.size == 1
+      [a, b, c]
+    else
+      [a.to_f, b.to_f, c.to_f]
     end
   end
 end
--- spec/triangle_classifier_spec.rb
+++ spec/triangle_classifier_spec.rb
@@ -6,6 +6,15 @@ describe "TriangleClassifier" do
     defined?(TriangleClassifier).should_not be_nil
   end
 
+  context "handling numeric input" do
+    it "should convert to one numeric type for all sides" do
+      TriangleClassifier.check_inputs(5, 5.0, 5.0).should eql([5.0, 5.0, 5.0])
+    end
+    it "should use the same values if the numeric type is the same" do
+      TriangleClassifier.check_inputs(5, 5, 5).should eql([5, 5, 5])
+    end
+  end
+
   context "invalid length sides" do
     it "should raise error when any length is zero or negative" do
       expect{TriangleClassifier.check_sides(0, 5, 5)}.to raise_error(ArgumentError)
@@ -19,15 +28,19 @@ describe "TriangleClassifier" do
 
   context "valid length sides" do
     it "should detect an equilateral triangle" do
-      TriangleClassifier.classify(5,5,5).should eql(:equilateral)
+      TriangleClassifier.classify(5, 5, 5).should eql(:equilateral)
     end
 
     it "should detect an isosceles triangle" do
-      TriangleClassifier.classify(1,5,5).should eql(:isosceles)
+      TriangleClassifier.classify(1, 5, 5).should eql(:isosceles)
     end
 
     it "should detect an scalene triangle" do
-      TriangleClassifier.classify(3,7,5).should eql(:scalene)
+      TriangleClassifier.classify(3, 7, 5).should eql(:scalene)
+    end
+
+    it "should detect an equilateral triangle" do
+      TriangleClassifier.classify(5, 5, 5.0).should eql(:equilateral)
     end
   end
 
